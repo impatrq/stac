4b6abf7ef76fb4d3979a4b36ecbbf43d2e81f3de
Manuel Torancio <manutorancio12@gmail.com>
2021-08-17 03:28:54 +0000

    style: alinear llaves consecuentes

--- LED1m.ino
+++ LED1m.ino
@@ -8,14 +8,16 @@
 #define echoPin 12  //Echo Ultrasonico -> pin 12
 #define ledRojo 10      //Led amarillo -> pin 11
 
-void setup() {
+void setup() 
+{
   Serial.begin (9600);  
   pinMode(trigPin, OUTPUT);  //Trig 
   pinMode(echoPin, INPUT);   //Echo 
   pinMode(ledRojo, OUTPUT);
 }
 
-void loop() {
+void loop() 
+{
   long duration, distance;
    // Mandar un pulso bajo para asegurar un pulso alto
   digitalWrite(trigPin, LOW);  
@@ -28,23 +30,26 @@ void loop() {
   //Convertir el tiempo en distancia
   distance = duration /29/2 ;
    
-  if (distance <= 100) {  
-    digitalWrite(ledRojo,HIGH);
+  if (distance <= 100)
+  {  
+    digitalWrite(ledRojo, HIGH);
     delay(100);
-    
-}
-  else {   
-    digitalWrite(ledRojo,LOW);
+  }
+  else
+  {   
+    digitalWrite(ledRojo, LOW);
     delay(100);
   }
   
-  if ( distance < 0){  
+  if (distance < 0)
+  {  
     Serial.println("fuera de alcance");
   }
-  else {
-    Serial.print( "distancia ");
+  else 
+  {
+    Serial.print("distancia ");
     Serial.print(distance);
-    Serial.println(" cm ");
+    Serial.println(" cm");
   }
   delay(100);  
 }
